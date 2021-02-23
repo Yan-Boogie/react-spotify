6f71954d96cdb84dad05d6cd8e9669c25a55ff8b
Ian Lai <ian24257587@gmail.com>
2021-02-23 13:28:27 +0800

    feat(spotify-api-request): build mock test

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,14 @@
-import * as React from 'react';
+import React from 'react';
 import { UserTokenContext } from './context';
 
-/**
- * @todo
- * Remove mock TOKEN
- */
-const TOKEN = `BQCMnFF8ObhVCl9JchjM7hcE4DHJTqSOKYqAD_5_VXVlRim_q9U3n2ogT0689Jv_
-GAQWLvRKviScO06OMx-WqajMF9-gBErNTwvXI_R11lPAUJKr753zPdbzXSHA-uJBvysllMGZvnW-m-VaLp3nr2eQC77icxfeN9MPiQbs7KjmQlJs`;
+import { TOKEN } from './mock';
 
-export const useSpotify = (token: string = TOKEN) => ({
-  Spotify: ({ children }: { children: () => React.ReactNode }) => (
-    <UserTokenContext.Provider value={token}>
-      {() => children()}
-    </UserTokenContext.Provider>
-  ),
-});
-
-export default useSpotify;
-
+export default function useSpotify(token: string = TOKEN) {
+  return {
+    Spotify: ({ children }: { children: () => React.ReactNode }) => (
+      <UserTokenContext.Provider value={token}>
+        {() => children()}
+      </UserTokenContext.Provider>
+    ),
+  };
+}
