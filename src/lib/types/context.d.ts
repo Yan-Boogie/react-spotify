79c18058fef0070775e87064a7129c94f86fc571
Ian Lai <ian24257587@gmail.com>
2021-02-24 19:55:04 +0800

    build(src-root): remove library

--- src/lib/types/context.d.ts
+++
@@ -1,2 +0,0 @@
-/// <reference types="react" />
-export declare const UserTokenContext: import("react").Context<string>;
--- src/lib/types/index.d.ts
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-export { default as useSpotifyApiRequest } from './SpotifyApiRequest';
-export declare function useSpotify(token?: string): {
-    Spotify: ({ children }: {
-        children: () => React.ReactNode;
-    }) => JSX.Element;
-};
