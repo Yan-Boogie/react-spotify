31f7775749780d8f96ce9015ad75714929996741
Ian Lai <ian24257587@gmail.com>
2021-04-05 15:57:39 +0800

    feat(root): build global css

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,28 +1,12 @@
-/* eslint-disable no-console */
-import { useSpotifyApiRequest, useSpotify } from 'src';
-import { TOKEN } from '../mock';
-
-const SpotifyPlaylist = () => {
-  const { isLoading, error, apiReturned } = useSpotifyApiRequest({
-    requestUrl: 'https://api.spotify.com/v1/me/playlists',
-    requestMethod: 'GET',
-  });
-
-  console.log('app data---->', isLoading, error, apiReturned);
-
-  return (
-    <div>
-      <span>testing</span>
-    </div>
-  );
-};
+import Link from 'next/link';
 
 export default function HomePage() {
-  const { Spotify } = useSpotify(TOKEN);
-
   return (
-    <Spotify>
-      <SpotifyPlaylist />
-    </Spotify>
+    <>
+      <div>Home</div>
+      <Link href="/spotify">
+        <a href="/spotify">Spotify</a>
+      </Link>
+    </>
   );
 }
