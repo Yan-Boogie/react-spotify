21cd809d1daf851071681159d3a25d305b18cfe5
Ian Lai <ian24257587@gmail.com>
2021-04-13 16:25:47 +0800

    feat(root): build storybook configure

---
+++ app/.storybook/main.js
@@ -0,0 +1,9 @@
+module.exports = {
+  stories: ['../src/**/*.stories.@(ts|tsx)'],
+  addons: [
+    '@storybook/addon-actions',
+    '@storybook/addon-controls',
+    '@storybook/addon-docs',
+    '@storybook/addon-viewport'
+  ],
+};
